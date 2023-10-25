0fba31157416382596f6b88955915c313227e0ea
Whendersson <142396727+Whendersson@users.noreply.github.com>
2023-10-25 20:31:24 -0300

    Add files via upload

---
+++ AtividadeMod7/src/Whendersson/Produto.java
@@ -0,0 +1,14 @@
+package Whendersson;
+
+public class Produto {
+
+	private int codigo;
+	
+	private int valor;
+	
+	private int lucro;
+	
+	private int quantidade;
+	
+	private String nome; 
+}
---
+++ AtividadeMod7/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module AtividadeMod7 {
+}
