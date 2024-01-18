66e17e586a2e20d79220c9e17c9e610b783b52e7
Whendersson <142396727+Whendersson@users.noreply.github.com>
2024-01-18 19:01:09 -0300

    Add files via upload

---
+++ AtividadeEBACmod9/src/exercicios/Casting.java
@@ -0,0 +1,17 @@
+package exercicios;
+/**
+ * @author Whendersson1
+ */
+public class Casting {
+	
+	public static void main(String[] args) {
+		
+		long num1 = 10;
+		System.out.println(num1);
+		
+		Long num2 = num1;
+		System.out.println(num2);
+				
+	}
+
+}
